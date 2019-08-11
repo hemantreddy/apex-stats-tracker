cbe8a33d5b45718044b6d469b7514dffcf649c66
Hemant Reddy <hemantreddy22@gmail.com>
2019-08-11 12:59:32 +0530

    complete backend

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load env
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+// Dev Logging
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Profile routes
+
+app.use('/api/v1/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
