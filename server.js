e6601f3f8857cfc7b81ad143c138da3a24997df7
Hemant Reddy <hemantreddy22@gmail.com>
2019-08-12 18:49:50 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 // Load env
 dotenv.config({ path: './config.env' });
@@ -18,6 +19,16 @@ if (process.env.NODE_ENV === 'development') {
 // Profile routes
 
 app.use('/api/v1/profile', require('./routes/profile'));
+
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8000;
 
