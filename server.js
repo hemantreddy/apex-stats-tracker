76e8b10aac5173ea4bece30521887fecfbc622c8
Hemant Reddy <hemantreddy22@gmail.com>
2019-08-12 20:26:31 +0530

    build

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const compression = require('compression');
 const path = require('path');
 
 // Load env
@@ -24,12 +23,10 @@ app.use('/api/v1/profile', require('./routes/profile'));
 // Serve static assets if in production
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression());
-  app.use(morgan('common'));
   // set static folder
-  app.use(express.static(path.resolve(__dirname, 'build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
